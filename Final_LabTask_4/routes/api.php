e3ab561ec1723278325935e6dda02c3ab4fa7876
fahim ahmmed mojumder <ahmmedahmmed75@gmail.com>
2022-12-07 00:33:01 +0600

    .

--- Final_LabTask_4/routes/api.php
+++ Final_LabTask_4/routes/api.php
@@ -21,7 +21,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-//api 
+// //api 
 // Route::get('/products/list', [ProductController::class, 'APIList'])->name('APIList');
 // Route::post('/products/list',[ProductController::class,'APIPost']);
 
